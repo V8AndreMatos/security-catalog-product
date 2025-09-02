051a822498039e560cb6ed9cd49bb1f04d9a828c
Andre <andreluisv8@gmail.com>
2025-09-02 19:52:49 -0300

    The first commit

---
+++ src/main/java/com/security/catalog/product/SecurityCatalogProductApplication.java
@@ -0,0 +1,13 @@
+package com.security.catalog.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityCatalogProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityCatalogProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/catalog/product/SecurityCatalogProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.catalog.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityCatalogProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
