48d93bf9e91153fbf51a63e233c47996b00b622e
Andre <andreluisv8@gmail.com>
2025-09-02 21:52:51 -0300

    Created class : ProductDTO

---
+++ src/main/java/com/security/catalog/product/dto/ProductDTO.java
@@ -0,0 +1,82 @@
+package com.security.catalog.product.dto;
+
+import com.security.catalog.product.entities.Product;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+
+    private List<CategoryDTO> categories =  new ArrayList<>();
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Long id, String name, String description, Double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+
+
+    }
+
+    public ProductDTO(Product entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.description = entity.getDescription();
+        this.price = entity.getPrice();
+
+    }
+
+    public ProductDTO(Product entity , Set<Category> categories) {
+        this(entity);
+        categories.forEach(cat -> this.categories.add(new CategoryDTO(cat)));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public List<CategoryDTO> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<CategoryDTO> categories) {
+        this.categories = categories;
+    }
+}
