c163f6f18bcc3f0d54bfd9815a61179d2455df79
Andre <andreluisv8@gmail.com>
2025-09-03 09:34:21 -0300

    Created class : UserRepository

---
+++ src/main/java/com/security/catalog/product/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.security.catalog.product.repositories;
+
+import com.security.catalog.product.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
